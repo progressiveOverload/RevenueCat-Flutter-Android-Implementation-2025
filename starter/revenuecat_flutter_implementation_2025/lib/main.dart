9b96f6df506e9a9ec037483ca7f2a9c3c36879b1
Ali Cihan Isikal <cihanisikal@outlook.com>
2025-01-18 17:06:08 +0300

    add RevenueCat configuration and constants for in-app purchases

--- starter/revenuecat_flutter_implementation_2025/lib/main.dart
+++ starter/revenuecat_flutter_implementation_2025/lib/main.dart
@@ -1,70 +1,46 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:purchases_flutter/purchases_flutter.dart';
 
-void main() {
+import 'constants.dart';
+import 'src/app.dart';
+import 'store_config.dart';
+
+void main() async {
+  if (Platform.isIOS || Platform.isMacOS) {
+    StoreConfig(
+      store: Store.appStore,
+      apiKey: appleApiKey,
+    );
+  } else if (Platform.isAndroid) {
+    // Run the app passing --dart-define=AMAZON=true
+    const useAmazon = bool.fromEnvironment("amazon");
+    StoreConfig(
+      store: useAmazon ? Store.amazon : Store.playStore,
+      apiKey: useAmazon ? amazonApiKey : googleApiKey,
+    );
+  }
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await _configureSDK();
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+Future<void> _configureSDK() async {
+  // Enable debug logs before calling `configure`.
+  await Purchases.setLogLevel(LogLevel.debug);
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  PurchasesConfiguration configuration;
+  if (StoreConfig.isForAmazonAppstore()) {
+    configuration = AmazonConfiguration(StoreConfig.instance.apiKey)
+      ..appUserID = null
+      ..purchasesAreCompletedBy = const PurchasesAreCompletedByRevenueCat();
+  } else {
+    configuration = PurchasesConfiguration(StoreConfig.instance.apiKey)
+      ..appUserID = null
+      ..purchasesAreCompletedBy = const PurchasesAreCompletedByRevenueCat();
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+  await Purchases.configure(configuration);
 }
