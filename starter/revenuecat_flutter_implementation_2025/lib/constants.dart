5558676143ea8d68715b8dea85f6ed07f5b1c615
Ali Cihan Isikal <cihanisikal@outlook.com>
2025-01-19 13:07:38 +0300

    a

--- starter/revenuecat_flutter_implementation_2025/lib/constants.dart
+++ starter/revenuecat_flutter_implementation_2025/lib/constants.dart
@@ -1,5 +1,5 @@
 //TO DO: add the entitlement ID from the RevenueCat dashboard that is activated upon successful in-app purchase for the duration of the purchase.
-const entitlementID = 'premium';
+const entitlementID = 'entl7cd7e5cd6c';
 
 //TO DO: add your subscription terms and conditions
 const footerText =
@@ -11,7 +11,7 @@ Read more about this here: https://www.revenuecat.com/blog/schedule-2-section-3-
 const appleApiKey = 'appl_api_key';
 
 //TO DO: add the Google API key for your app from the RevenueCat dashboard: https://app.revenuecat.com
-const googleApiKey = 'googl_api_key';
+const googleApiKey = 'goog_htGFjCzmZduhPSqVlpVEFZoOrEU';
 
 //TO DO: add the Amazon API key for your app from the RevenueCat dashboard: https://app.revenuecat.com
 const amazonApiKey = 'amazon_api_key';
